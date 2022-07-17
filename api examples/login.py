af4fb383bd8786505eee4cea6de0b1b2b8d33eed
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-07-17 15:29:12 -0400

    upload login

---
+++ api examples/login.py
@@ -0,0 +1,10 @@
+import requests
+
+session = requests.session()
+
+data = {'username': 'exampleLogin123', 'password': 'examplePassword123'}
+
+request = session.post('https://yiffer.xyz/api/login', data=data)
+
+print(request.text) # Prints the response of the server
+print(request.cookies["yiffer_session"]) # Prints the cookie
